56c125a5c2322eefa2a867227094190f76e55c0d
Vanessa Bou <rvbouu@gmail.com>
2024-04-06 13:44:39 -0500

    Updated formatting to logic JS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,19 +7,10 @@ const span = document.querySelectorAll('span');
 const blogName = document.querySelector('#blogName'); // only index.html
 const backBtn = document.querySelector('#back-btn'); // only blog.html
 
-// let toggleDark = localStorage.setItem('toggleDark',toggle.textContent);
-
-
-
-
-
-
 // Dark Mode function
-
-
 // gets localStorage data and sets screen to last setting
-darkMode = localStorage.getItem('darkMode') || 'light'; // if key isn't found in localStorage, sets darkMode to 'light'
-toggleDark = localStorage.getItem('toggleDark') || '☀️';
+const darkMode = localStorage.getItem('darkMode') || 'light'; // if key isn't found in localStorage, sets darkMode to 'light'
+const toggleDark = localStorage.getItem('toggleDark') || '☀️';
 body.classList.add(darkMode);
 header.classList.add(darkMode);
 footer.classList.add(darkMode);
